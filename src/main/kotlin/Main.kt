b5609757e9d5d7f9c986f6c8491aa1f6d4c91267
corffen <corffen@gmail.com>
2023-07-17 21:06:19 +0800

    添加 add函数

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,3 +5,7 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+fun add(){
+    println("add feature-1")
+}
