fa55e660f3c6ffe7a8aa81102ff5540d509d1693
gordon_kara <corffen@gmail.com>
2023-08-13 17:22:44 +0800

    新增 client方法的实现

--- src/main/kotlin/com/study/pattern/abs_factory/Client.kt
+++ src/main/kotlin/com/study/pattern/abs_factory/Client.kt
@@ -2,4 +2,20 @@ package com.study.pattern.abs_factory
 
 class Client {
 
+    fun add(){
+        println("add 1")
+    }
+    fun delete(){
+        println("delete 1")
+        println("delete 2")
+    }
+
+    fun modify(){
+        println("modify 1")
+        println("modify 2")
+        println("modify 3")
+    }
+    fun find(){
+        println("find 1")
+    }
 }
